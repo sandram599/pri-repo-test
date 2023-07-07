7ef37ab0e53d36bebdac175f50c43c6f57d5c1f3
sandram599 <137435289+sandram599@users.noreply.github.com>
2023-07-06 21:39:53 -0500

    Create Main.java

---
+++ main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import controller.JPaintController;
+import model.persistence.ApplicationState;
+import view.ClickHandler;
+import view.gui.Gui;
+import view.gui.GuiWindow;
+import view.gui.PaintCanvas;
+import view.interfaces.IGuiWindow;
+import view.interfaces.IUiModule;
+import java.awt.event.MouseListener;
+
+public class Main {
+    public static void main(String[] args){
+        PaintCanvas paintCanvas = new PaintCanvas();
+
+        IGuiWindow guiWindow = new GuiWindow(paintCanvas);
+        IUiModule uiModule = new Gui(guiWindow);
+
+        ApplicationState appState = new ApplicationState(uiModule); 
+
+        new JPaintController(uiModule, appState);
+        
+        paintCanvas.addMouseListener(new ClickHandler(paintCanvas));
+    }
+}
