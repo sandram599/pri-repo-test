fd34f0c219a64d27887e4e5a8a183a5fe70a19a4
sandram599 <137435289+sandram599@users.noreply.github.com>
2023-07-06 21:47:23 -0500

    Add files via upload

---
+++ model/pointClass.java
@@ -0,0 +1,21 @@
+package model;
+
+public class pointClass {
+	
+	public int x;
+	public int y;
+	
+	public pointClass (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+}
+	
