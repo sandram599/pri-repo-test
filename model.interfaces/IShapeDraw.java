3c43eb3cfe9b53ed5cf0cbdd2454b65df2eb70f4
sandram599 <137435289+sandram599@users.noreply.github.com>
2023-07-22 23:18:27 -0500

    Create IShapeDraw.java

---
+++ model.interfaces/IShapeDraw.java
@@ -0,0 +1,15 @@
+package model.interfaces;
+
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.util.ArrayList;
+
+import controller.CreateShape;
+
+public interface IShapeDraw { //interface to draw all shapes
+
+	void designShape(CreateShape shape);
+
+	
+	
+}
