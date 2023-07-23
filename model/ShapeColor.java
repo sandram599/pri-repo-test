91caa25d5c87361e7c66f7f63e253a6b2311f8a4
sandram599 <137435289+sandram599@users.noreply.github.com>
2023-07-22 23:27:28 -0500

    Update ShapeColor.java

--- model/ShapeColor.java
+++ model/ShapeColor.java
@@ -1,17 +1,30 @@
 package model;
 
+import java.awt.Color;
+
 public enum ShapeColor {
-    BLACK,
-    BLUE,
-    CYAN,
-    DARK_GRAY,
-    GRAY,
-    GREEN,
-    LIGHT_GRAY,
-    MAGENTA,
-    ORANGE,
-    PINK,
-    RED,
-    WHITE,
-    YELLOW
+    BLACK(Color.BLACK),
+    BLUE(Color.BLUE),
+    CYAN(Color.CYAN),
+    DARK_GRAY(Color.DARK_GRAY),
+    GRAY(Color.GRAY),
+    GREEN(Color.GREEN),
+    LIGHT_GRAY(Color.LIGHT_GRAY),
+    MAGENTA(Color.MAGENTA),
+    ORANGE(Color.ORANGE),
+    PINK(Color.PINK),
+    RED(Color.RED),
+    WHITE(Color.WHITE),
+    YELLOW(Color.YELLOW);	
+
+	private Color c;
+
+	ShapeColor(Color c) {
+		this.c = c;
+	}
+
+	public Color getShapeColor() {
+		return c;
+	}
+
 }
