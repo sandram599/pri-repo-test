72daf3b68e5c6b5d020ee92a4fc0c5eb9749e77d
sandram599 <137435289+sandram599@users.noreply.github.com>
2023-07-22 23:23:22 -0500

    Update PaintCanvas.java

--- view.gui/PaintCanvas.java
+++ view.gui/PaintCanvas.java
@@ -1,39 +1,60 @@
 package view.gui;
 
 import javax.swing.JComponent;
+
+import controller.CreateShape;
+import model.ShapesList;
+import model.interfaces.IShapeDraw;
+import model.interfaces.IShapeSubject;
+
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 public class PaintCanvas extends JComponent {
+	
+	public Graphics2D getGraphics2D() {
+		return (Graphics2D) getGraphics();
+	}
 
-    Point startPoint = new Point();
-	Point endPoint = new Point();
-	private int width;
-	private int height;
+	private ShapesList shapeList;
+	private CreateShape newshape;
+	
+	public void paint(Graphics2D g) {
+		//super.paint(g);
+		Graphics2D g2d = (Graphics2D) g.create();
+		for(CreateShape shape : shapeList.getshapelist()) {
+		shape.makeShape(newshape);
+		}
+	}
+	
+}
 
-	@Override
-    public void paint(Graphics g) {
-    	
-    	Graphics2D graphics2d = (Graphics2D)g;
+
+
+
+      // Graphics2D graphics2d = (Graphics2D) g;
+       
 
         // Draw all shapes here
 
-        // For example purposes only; remove all lines below from your final project.
-		width = endPoint.x - startPoint.x;
-		height = endPoint.x - startPoint.y;
-        graphics2d.setColor(Color.GREEN);
-		graphics2d.fillRect(startPoint.x, endPoint.y, width, height); 
-		graphics2d.drawRect(startPoint.x, endPoint.y, width, height); 
+        /* For example purposes only; remove all lines below from your final project.
+       // graphics2d.setColor(Color.GREEN);
+        //graphics2d.fillRect(12, 13, 200, 400);
+        graphics2d.fillRect(0, 0, getHeight(), getWidth());
+		graphics2d.drawRect(0, 0, getHeight(), getWidth());
+		//graphics2d.drawRect(12, 13, 200, 400);
+
 
         // Outlined rectangle
-        //graphics2d.setStroke(new BasicStroke(5));
-        //graphics2d.setColor(Color.BLUE);
-        //graphics2d.drawRect(12, 13, 200, 400);
+        graphics2d.setStroke(new BasicStroke(5));
+        graphics2d.setColor(Color.BLUE);
+        graphics2d.drawRect(12, 13, 200, 400);
 
         // Selected Shape
-        //Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[]{9}, 0);
-        //graphics2d.setStroke(stroke);
-        //graphics2d.setColor(Color.BLACK);
-        //graphics2d.drawRect(7, 8, 210, 410); //unsure what this will be here the values
-    }
-}
+        Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[]{9}, 0);
+        graphics2d.setStroke(stroke);
+        graphics2d.setColor(Color.BLACK);
+        graphics2d.drawRect(7, 8, 210, 410);*/
+    
+
