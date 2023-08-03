9895c5bb206dc8f9fa430c1c526b2510519fb046
sandram599 <137435289+sandram599@users.noreply.github.com>
2023-08-03 17:12:54 -0500

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -1,6 +1,20 @@
 package main;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+
 import controller.JPaintController;
+import controller.ShapeBuilder;
+import controller.createshapeCommand;
+import model.ShapeColor;
+import model.ShapeList;
+import model.interfaces.IApplicationState;
+import model.interfaces.IShape;
+import model.interfaces.IShapeObserver;
+import model.interfaces.IShapeSubject;
+import model.interfaces.Shape;
 import model.persistence.ApplicationState;
 import view.ClickHandler;
 import view.gui.Gui;
@@ -8,29 +22,30 @@ import view.gui.GuiWindow;
 import view.gui.PaintCanvas;
 import view.interfaces.IGuiWindow;
 import view.interfaces.IUiModule;
-import java.awt.event.MouseListener;
 
 public class Main {
-    public static void main(String[] args){
-        PaintCanvas paintCanvas = new PaintCanvas();
 
+
+	public static void main(String[] args){
+		
+        final List<IShape> list = new ArrayList<IShape>();
+        ShapeList sList = new ShapeList(list);
+		
+		ShapeBuilder shapebuilder = new ShapeBuilder();
+        PaintCanvas paintCanvas = new PaintCanvas(sList);
         IGuiWindow guiWindow = new GuiWindow(paintCanvas);
         IUiModule uiModule = new Gui(guiWindow);
 
-        ApplicationState appState = new ApplicationState(uiModule); 
+
+        ApplicationState appState = new ApplicationState(uiModule);
 
         new JPaintController(uiModule, appState);
         
-        paintCanvas.addMouseListener(new ClickHandler(paintCanvas));
-
-        final List<CreateShape> list = new ArrayList<CreateShape>();
-        IShapeSubject sList = new ShapesList(list);
+        paintCanvas.addMouseListener(new ClickHandler(paintCanvas, appState, sList));
         
-        IShapeObserver observers = new CreateShape(paintCanvas);
+        
+        IShapeObserver observers = new createshapeCommand(appState, paintCanvas, sList, shapebuilder);
         sList.registerObservers(observers);
         
         }        
-}
-
-    }
 }
