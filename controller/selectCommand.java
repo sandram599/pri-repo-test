218c5a70c644fe77ed3bc9b8874689379d0ab3f1
sandram599 <137435289+sandram599@users.noreply.github.com>
2023-08-03 17:07:28 -0500

    Update selectCommand.java

--- controller/selectCommand.java
+++ controller/selectCommand.java
@@ -1,33 +1,64 @@
 package controller;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 import model.Point;
-import model.ShapesList;
+import model.ShapeList;
 import model.interfaces.ICommand;
+import model.interfaces.IShape;
+import model.interfaces.IShapeObserver;
 import model.interfaces.IUndoable;
+import model.interfaces.Shape;
 import model.persistence.ApplicationState;
 import view.gui.PaintCanvas;
 
-public class selectCommand implements ICommand {
+public class selectCommand implements ICommand, IShapeObserver {
 	
-	CreateShape shape, selectedShape;
-	private PaintCanvas paintCanvas;
-	private ShapesList shapeList;
-	static boolean ifSelected = false;
-	Point startPoint, endPoint;
-	
-	
-	public selectCommand(ShapesList shapeList, PaintCanvas paintCanvas) {
-		this.shapeList = shapeList;
+	PaintCanvas paintCanvas;
+	private Point startPoint, endPoint, xPoint, yPoint;
+	private int height, width;
+	private ShapeList shapeList;
+
+	public selectCommand(Point startPoint, Point endPoint, PaintCanvas paintCanvas, ShapeList shapeList) {
+		this.startPoint = startPoint;
+		this.endPoint = endPoint;
 		this.paintCanvas = paintCanvas;
+		this.shapeList = shapeList;
+		
+		xPoint = new Point(startPoint.getX(), endPoint.getX());
+		yPoint =  new Point(startPoint.getY(), endPoint.getY());
+		
+		width = startPoint.getX() - endPoint.getX();
+		height = startPoint.getY() - endPoint.getY();
 	}
+
 
 	@Override
 	public void run() {
-		for(CreateShape shape : shapeList.getshapelist()) {
-		if(shape.isIn(shape.grabStart())) { 
-			ifSelected = true; selectedShape = shape;
-			shapeList.addSelectShape(selectedShape);
+		//collision detection for bounding box		
+		shapeList.selectedshapeList().clear();
+		for(IShape shape : shapeList.getshapelist()) { 
+			
+			if(shape.getStartPoint().getX() < xPoint.getX() + width && 
+			shape.getStartPoint().getX() + shape.getWidth() + shape.getWidth() 
+			  > xPoint.getX() && shape.getStartPoint().getY() < yPoint.getY() 
+			+ height && shape.getStartPoint().getY() + shape.getHeight() > 
+			  yPoint.getY()) {
+				
+			shapeList.selectedshapeList().add(shape);	
 			}
-		}	
+		}
+			paintCanvas.repaint();
+	}
+
+
+	@Override
+	public void update() {
+		Graphics2D graphics2d = (Graphics2D) paintCanvas.getGraphics();
+		graphics2d.setColor(Color.WHITE);
+		graphics2d.fillRect(0, 0, paintCanvas.getWidth(), paintCanvas.getHeight());
+		paintCanvas.repaint();	
 	}
 }
+	
