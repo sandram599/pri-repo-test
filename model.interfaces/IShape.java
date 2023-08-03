200e766d1d1bc69afc533961ac73f1eb8a28c84f
sandram599 <137435289+sandram599@users.noreply.github.com>
2023-08-03 17:17:54 -0500

    Create IShape.java

---
+++ model.interfaces/IShape.java
@@ -0,0 +1,31 @@
+package model.interfaces;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import controller.ShapeBuilder;
+import model.Point;
+import model.ShapeColor;
+import model.ShapeShadingType;
+import model.ShapeType;
+import model.persistence.ApplicationState;
+import model.ShapeList;
+
+public interface IShape {
+	
+	int getX();
+	int getY();
+	int getHeight();
+	int getWidth();
+	Point getStartPoint();
+	Point getEndPoint();
+	ShapeColor getPrimary();
+	ShapeColor getSecondary();
+	ShapeType getShapeType();
+	ShapeShadingType getShadeType();	
+	void draw(Graphics2D g); 
+	ShapeBuilder getShapeBuilder();
+	void move(int xDelta, int yDelta);
+	void shapeOutline(Graphics2D g);
+	ShapeList getshapeList();
+}
