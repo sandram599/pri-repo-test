93712c12f508abeb4ce6de12d45c67c34ade365e
sandram599 <137435289+sandram599@users.noreply.github.com>
2023-08-10 00:27:08 -0500

    Update ShapesList.java

--- model/ShapesList.java
+++ model/ShapesList.java
@@ -15,9 +15,10 @@ import view.gui.PaintCanvas;
 public class ShapeList implements IShapeSubject {
 	
 	private List<IShapeObserver> shapeObservers = new ArrayList<IShapeObserver>();
-	private List<IShape> shapeList  = new ArrayList<IShape>(); ;
-	private List<IShape> selectedshapesList = new ArrayList<IShape>();
-	private List<IShape> clipBoard = new ArrayList<IShape>(); //move to notifyobservers
+	private List<IShape> shapeList  = new ArrayList<IShape>(); 
+	private List<IShape> selectedshapeList = new ArrayList<IShape>();
+	private List<IShape> clipBoard = new ArrayList<IShape>(); 
+	private List<IShape> pasteList = new ArrayList<IShape>();
 	
 	private PaintCanvas paintCanvas;
 	
@@ -37,8 +38,6 @@ public class ShapeList implements IShapeSubject {
 	@Override
 	public void add(IShape shape) {
 		shapeList.add(shape);
-		selectedshapesList.add(shape);
-		clipBoard.add(shape);
 		notifyObservers();		
 	}
 
@@ -52,8 +51,6 @@ public class ShapeList implements IShapeSubject {
 	@Override
 	public void remove(IShape shape) {
 		shapeList.remove(shape);
-		selectedshapesList.remove(shape);
-		clipBoard.remove(shape);
 		notifyObservers();
 	}
 
@@ -61,16 +58,28 @@ public class ShapeList implements IShapeSubject {
 		return shapeList;
 	}
 	
-	public List<IShape> getselectedshapesList() {
-		return selectedshapesList;
+	public List<IShape> getselectedshapeList() {
+		return getSelectedshapeList();
 	}
 	
 	public List<IShape> selectedshapeList(){
-		return selectedshapesList;
+		return getSelectedshapeList();
 	}
 	
 	@Override
 	public List<IShape> getClipBoard() {
+		return clipBoard;		
+	}
+
+	public List<IShape> clipBoard() {
 		return clipBoard;
 	}
+	public List<IShape> getSelectedshapeList() {
+		return selectedshapeList;
+	}
+
+	public List<IShape> pasteList() {
+		return pasteList;
+	}
+
 }
