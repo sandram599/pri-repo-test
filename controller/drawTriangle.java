b927c284f5831d51a1a116af10b0e4551287400f
sandram599 <137435289+sandram599@users.noreply.github.com>
2023-08-17 23:11:09 -0500

    Update drawTriangle.java

--- controller/drawTriangle.java
+++ controller/drawTriangle.java
@@ -4,6 +4,7 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
+import java.util.ArrayList;
 
 import model.Point;
 import model.ShapeColor;
@@ -17,7 +18,7 @@ import view.gui.PaintCanvas;
 
 public class drawTriangle implements IShape {
 	
-	private ShapeBuilder shapebuilder;
+	private shapeBuilder shapebuilder;
 	private ApplicationState appState;
 	PaintCanvas paintCanvas;
 	private int[] xPoints = new int[3]; //new coordinates
@@ -26,20 +27,15 @@ public class drawTriangle implements IShape {
 	ShapeColor primary, secondary;
 	public ShapeShadingType shade;
 	private ShapeType shapetype;
-	private int height, width, x, y;
+	private int height, width;
 	private Point xPoint, yPoint;
-	private int updateX, updateY;
-	private ShapeList shapeList;
-	private int[] xtemp;
-	private int[] ytemp;
-	private IShape shape;
-	
+	private ShapeList shapeList;	
 	
 	public drawTriangle(Point startPoint, Point endPoint, ShapeColor primary, ShapeColor secondary,
 			ShapeShadingType shade, ShapeType shapetype, int height, int width) {
 		
-		this.startPoint = startPoint;
-		this.endPoint = endPoint;
+		this.startPoint = new Point(startPoint.x, startPoint.y);
+		this.endPoint = new Point(endPoint.x, endPoint.y);
 		this.primary = primary;
 		this.secondary = secondary;
 		this.shapetype = shapetype;
@@ -47,19 +43,17 @@ public class drawTriangle implements IShape {
 		this.height = height;
 		this.width = width;
 		
-		//new x coordinates
-		xPoints[0] = startPoint.getX();
-		xPoints[1] = startPoint.getX();
-		xPoints[2] = endPoint.getX();
-		
-		//new y coordinates
-		yPoints[0] = startPoint.getY();
-		yPoints[1] = endPoint.getY();
-		yPoints[2] = endPoint.getY();
-		
+		//x coordinates		
+		xPoints[0] = startPoint.x;
+		xPoints[1] = startPoint.x;
+		xPoints[2] = endPoint.x;
+	
+		//y coordinates
+		yPoints[0] = startPoint.y;
+		yPoints[1] = endPoint.y;
+		yPoints[2] = endPoint.y;
 		
 	}
-	
 	
 	public void draw(Graphics2D g2d) {
 				
@@ -85,58 +79,49 @@ public class drawTriangle implements IShape {
 	}
 
 	@Override
-	public ShapeBuilder getShapeBuilder() { 
+	public shapeBuilder getShapeBuilder() { 
 		return shapebuilder;
 	}
 
-
 	@Override
 	public int getX() {
-		return x;
+		return startPoint.x;
 	}
-
 
 	@Override
 	public int getY() {
-		return y;
+		return startPoint.y;
 	}
-
 
 	@Override
 	public int getHeight() {
 		return height;
 	}
 
-
 	@Override
 	public int getWidth() {
 		return height;
 	}
-
 
 	@Override
 	public Point getStartPoint() {
 		return startPoint;
 	}
 
-
 	@Override
 	public Point getEndPoint() {
 		return endPoint;
 	}
-
 
 	@Override
 	public ShapeColor getPrimary() {
 		return primary;
 	}
 
-
 	@Override
 	public ShapeColor getSecondary() {
 		return secondary;
 	}
-
 
 	@Override
 	public ShapeType getShapeType() {
@@ -148,14 +133,6 @@ public class drawTriangle implements IShape {
 		return shade;
 	}
 	
-	public int updateX() {
-		return startPoint.getX();
-	}
-	
-	public int updateY() {
-		return startPoint.getY();
-	}
-
 	@Override
 	public void move(int xDelta, int yDelta) {
 		
@@ -178,9 +155,25 @@ public class drawTriangle implements IShape {
 
 	@Override
 	public void outline(Graphics2D g) {
+		
+		//outline x coordinates
+		xPoints[0] = xPoints[0] - 8;
+		xPoints[1] = xPoints[1] - 8;
+		xPoints[2] = xPoints[2] + 15;
+		
+		//outline y coordinates
+		yPoints[0] = yPoints[0] - 15;
+		yPoints[1] = yPoints[1] + 8;
+		yPoints[2] = yPoints[2] + 8;
+		
 		Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[]{9}, 0);
 		g.setStroke(stroke);
 		g.setColor(Color.BLACK);
-		g.drawPolygon(xPoints, yPoints, 3);	
+		g.drawPolygon(xPoints , yPoints, 3);	
+	}
+
+	@Override
+	public ArrayList<IShape> getChildren() {
+		return null;
 	}
 }
