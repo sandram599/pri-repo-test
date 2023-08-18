f67361fc3d06ff461307c2692b3aa20af809bbb0
sandram599 <137435289+sandram599@users.noreply.github.com>
2023-08-17 23:10:01 -0500

    Update drawRectangle.java

--- controller/drawRectangle.java
+++ controller/drawRectangle.java
@@ -20,42 +20,30 @@ import view.gui.PaintCanvas;
 
 public class drawRectangle implements IShape { 
 	
-	private Point startPoint, endPoint, xPoint, yPoint;
-	private int height, width, x, y, xDelta, yDelta;
+	private Point startPoint, endPoint;
+	private int height, width, xDelta, yDelta;
 	ShapeColor primary, secondary;
 	private ShapeShadingType shade;
 	private ShapeType shapetype;
-	private ShapeBuilder shapebuilder;
+	private shapeBuilder shapebuilder;
 	private ApplicationState appState;
 	PaintCanvas paintCanvas;
-	private int updateX;
-	private int updateY;
 	private ShapeList shapeList;
-	private int X1, Y1, Y2, X2;
-	private IShape shape;
-	MouseMode mouse;
 
 	
 	public drawRectangle (Point startPoint, Point endPoint, ShapeColor primary, ShapeColor secondary,
 			ShapeShadingType shade, ShapeType shapetype, int height, int width) {
 
-		this.startPoint = startPoint;
-		this.endPoint = endPoint;
+		this.startPoint = new Point(startPoint.x, startPoint.y);
+		this.endPoint = new Point(endPoint.x, endPoint.y) ;
 		this.primary = primary;
 		this.secondary = secondary;
 		this.shapetype = shapetype;
 		this.shade = shade;
 		this.height = height;
 		this.width = width;		
-
-		
-	X1 = Math.min(startPoint.getX(), endPoint.getX()); 
-	X2 = Math.max(startPoint.getX(), endPoint.getX()); 
-        Y1 = Math.min(startPoint.getY(), endPoint.getY()); 
-        Y2 = Math.max(startPoint.getY(), endPoint.getY());
 	}
 	
-
 	@Override
 	public void draw(Graphics2D g2d) { 
 		
@@ -63,34 +51,34 @@ public class drawRectangle implements IShape {
 		
 		case FILLED_IN: 
 			g2d.setColor(primary.grabColors());
-			g2d.fillRect(X1, Y1, width, height);
+			g2d.fillRect(startPoint.x, startPoint.y, width, height);
 			break;
 		case OUTLINE: 
 			g2d.setColor(primary.grabColors());
 			g2d.setStroke(new BasicStroke(5));
-			g2d.drawRect(X1, Y1, width, height);
+			g2d.drawRect(startPoint.x, startPoint.y, width, height);
 			break;
 		case OUTLINE_AND_FILLED_IN:
 			g2d.setColor(primary.grabColors());
-			g2d.fillRect(X1, Y1, width, height);
+			g2d.fillRect(startPoint.x, startPoint.y, width, height);
 			g2d.setStroke(new BasicStroke(5));
 			g2d.setColor(secondary.grabColors());
-			g2d.drawRect(X1, Y1 , width, height);
+			g2d.drawRect(startPoint.x, startPoint.y , width, height);
 			break;	
 		}
 	}
 	
 	@Override
-	public ShapeBuilder getShapeBuilder() {
+	public shapeBuilder getShapeBuilder() {
 		return shapebuilder;
 	}
 	@Override
 	public int getX() {
-		return x;
+		return startPoint.x;
 	}
 	@Override
 	public int getY() {
-		return y;
+		return startPoint.y;
 	}
 	@Override
 	public int getHeight() {
@@ -127,8 +115,8 @@ public class drawRectangle implements IShape {
 	
 	@Override
 	public void move(int xDelta, int yDelta) {
-		X1 = X1 + xDelta;
-		Y1 = Y1 + yDelta;		
+		startPoint.x = startPoint.x + xDelta;
+		startPoint.y = startPoint.y + yDelta;		
 	}
 
 	@Override
@@ -137,10 +125,15 @@ public class drawRectangle implements IShape {
 	}
 
 	@Override
-	public void outline(Graphics2D g) {
+	public void outline(Graphics2D g) { 
 		Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[]{9}, 0);
 		g.setStroke(stroke);
 		g.setColor(Color.BLACK);
-		g.drawRect(X1 + 2, Y1 + 2, width, height);
+		g.drawRect(startPoint.x - 6, startPoint.y - 6, width + 12, height + 12);
 	}
+
+	@Override
+	public ArrayList<IShape> getChildren() {
+		return null;
+	}	
 }
